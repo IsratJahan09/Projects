1cbabdcf23eb071738fef23548ff923adc962577
Israt Jahan <140290748+IsratJahan09@users.noreply.github.com>
2025-10-18 22:44:11 +0600

    Add menu toggle functionality to myscript.js

---
+++ Trafalgar landing page/myscript.js
@@ -0,0 +1,7 @@
+const q = document.querySelector(".menu-icon")
+const ul = document.querySelector(".ull")
+q.addEventListener('click', ()=>{
+    // event.preventDefault()
+    ul.classList.toggle('show')
+
+})
